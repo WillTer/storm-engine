d49f92f6647666d4891639b74be67fe856af063a
Andrei Shtabel <ashtabel94@gmail.com>
2025-06-27 08:29:28 +0700

    Ignore pedantic warnings for external libs

--- src/libs/renderer_next/hlslpp.h
+++ src/libs/renderer_next/hlslpp.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#ifndef _MSC_VER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+
 #define HLSLPP_FEATURE_TRANSFORM
 #include <hlsl++.h>
+
+#ifndef _MSC_VER
+#pragma GCC diagnostic pop
+#endif
 
 namespace hlsl
 {
